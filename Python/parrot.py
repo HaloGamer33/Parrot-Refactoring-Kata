1938100a4ba482efc37891d7292b026e4701b36e
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-05-17 22:06:16 -0600

    feat: create EuropeanParrot class

--- Python/parrot.py
+++ Python/parrot.py
@@ -41,3 +41,11 @@ class Parrot:
 
     def _base_speed(self):
         return 12.0
+
+
+class EuropeanParrot:
+    def speed(self):
+        return self._base_speed()
+
+    def cry(self):
+        return "Sqoork!"
