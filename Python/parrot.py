c1a9238a81148b42cb6965b481e3581e3fde13b6
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-05-17 22:48:26 -0600

    refactor: remove not needed constructor for Parrot base class

--- Python/parrot.py
+++ Python/parrot.py
@@ -10,12 +10,6 @@ class ParrotType(Enum):
 
 class Parrot:
 
-    def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
-        self._type = type_of_parrot
-        self._number_of_coconuts = number_of_coconuts
-        self._voltage = voltage
-        self._nailed = nailed
-
     @abstractmethod
     def speed(self):
         pass
