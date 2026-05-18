aac68abc9fed2ae4f686319e9c152218cfd2e111
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-05-17 22:32:53 -0600

    feat: redefine AfricanParrot constructor

--- Python/parrot.py
+++ Python/parrot.py
@@ -56,6 +56,9 @@ class EuropeanParrot(Parrot):
 
 
 class AfricanParrot(Parrot):
+    def __init__(self, number_of_coconuts):
+        self._number_of_coconuts = number_of_coconuts
+
     def speed(self):
         return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
 
